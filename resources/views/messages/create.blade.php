200e10a597dbb25ae55ac94124b6fad5b709b3f4
jiro.kirameki <jo.ki@e-ottimo.com>
2018-05-25 09:18:20 +0000

    bootstrap design

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -5,18 +5,25 @@
 <h1>メッセージ新規作成ページ</h1>
 
 
-    {!! Form::model($message, ['route' => 'messages.store']) !!}
+<div class="row">
+        <div class="col-xs-6">
+        {!! Form::model($message, ['route' => 'messages.store']) !!}
         
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
-
+        <div class="form-group">
+            {!! Form::label('title', 'タイトル:') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+       
+       <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
 
         {!! Form::submit('投稿') !!}
 
-    {!! Form::close() !!}
+        {!! Form::close('投稿', ['class' => 'btn btn-primary']) !!}
     
-    
+        
+        </div>
+</div>
 
 @endsection
